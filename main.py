81d51c5517ef01c6ed7345a85af65d0436bcdf82
Sergio Caballero <sergio_jair4@hotmail.com>
2023-04-16 20:31:41 -0500

    commit-2

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-nombre_archivo = input("Ingresa el nombre del archivo: ")
+import re
+from math import ceil
 
-try:
-    with open(nombre_archivo, 'r') as archivo:
-        contenido = archivo.read()
-        print(contenido)
-except FileNotFoundError:
-    print("El archivo no existe")
-except:
-    print("Ocurrió un error al abrir el archivo")
+patron = r"(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BITA|bita|BITB|bitb|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|SUBA|suba|SUBB|subb|SUBD|subd)"
 
-IMM=r"^(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BITA|bita|BITB|bitb|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|SUBA|suba|SUBB|subb|SUBD|subd)"
-mnemonicos = re.findall(IMM, contenido)
+with open('START.ASC', 'r', buffering=1024) as archivo:
+    for linea in archivo:
+        #Buscar coincidencias del patrón en la línea actual
+        if re.search(patron, linea):
+            #Realizar acciones con las coincidencias encontradas
+            print(linea)
 
-# Imprime los mnemónicos de 68HC11 encontrados
-print("Mnemónicos encontrados:", mnemonicos)
+    for linea2 in archivo:    
+        if "#'" in linea2:
+            index = linea2.find("#'") +2
+            simbolos = linea2[index:-1]
+            for simbolo in simbolos:
+                valor_decimal = ord(simbolo)
+                print(valor_decimal)

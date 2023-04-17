e7659a09f025d38b6a8e53f3eddf0e0e8c8e10e8
Sergio Caballero <sergio_jair4@hotmail.com>
2023-04-16 22:03:34 -0500

    .

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import re
-from math import ceil
 
-patron = r"(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BITA|bita|BITB|bitb|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|SUBA|suba|SUBB|subb|SUBD|subd)"
+ASCII="^(?|A)"
+IMM="^(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BITA|bita|BITB|bitb|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|SUBA|suba|SUBB|subb|SUBD|subd)"
+DIR="^(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BCLR|bclr|BITA|bita|BITB|bitb|BRCLR|brclr|BRSET|brset|BSET|bset|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|JSR|jsr|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|STAA|staa|STAB|stab|STD|std|STS|sts|STX|stx|STY|sty|SUBA|suba|SUBB|subb|SUBD|subd)"
 
-with open('START.ASC', 'r', buffering=1024) as archivo:
-    for linea in archivo:
-        #Buscar coincidencias del patrón en la línea actual
-        if re.search(patron, linea):
-            #Realizar acciones con las coincidencias encontradas
-            print(linea)
+nombre_archivo ="START.ASC"
 
-    for linea2 in archivo:    
-        if "#'" in linea2:
-            index = linea2.find("#'") +2
-            simbolos = linea2[index:-1]
+def Encontrar_ascii(lineas):
+    if "#'" in linea:
+            index = linea.find("#'") +2
+            simbolos = linea[index:-1]
             for simbolo in simbolos:
                 valor_decimal = ord(simbolo)
                 print(valor_decimal)
+    return ascii
+
+patron = "(ADCA|adca|ADCB|adcb|ADDA|adda|ADDB|addb|ADDD|addd|ANDA|anda|ANDB|andb|BITA|bita|BITB|bitb|CMPA|cmpa|CMPB|cmpb|CPD|cpd|CPX|cpx|CPY|cpy|EORA|eora|EORB|eorb|LDAA|ldaa|LDAB|ldab|LDD|ldd|LDS|lds|LDX|ldx|LDY|ldy|ORAA|oraa|ORAB|orab|SBCA|sbca|SBCB|sbcb|SUBA|suba|SUBB|subb|SUBD|subd)"
+
+with open('START.ASC', 'r', buffering=1024) as archivo:
+    for linea in archivo:
+        # Buscar coincidencias del patrón en la línea actual
+        if re.search(patron, linea):
+            # Realizar acciones con las coincidencias encontradas  
+            print(linea)  
+            Encontrar_ascii(linea)
